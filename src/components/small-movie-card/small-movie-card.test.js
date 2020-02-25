9f3c58c33837f70e71aac752c004250573c40e8c
pavelkarev <pavel.karev@pegast.ru>
2020-02-25 16:53:50 +0300

    Use VideoPlayer in SmallMovieCard

--- src/components/small-movie-card/small-movie-card.test.js
+++ src/components/small-movie-card/small-movie-card.test.js
@@ -6,11 +6,14 @@ const movie = {
   id: 1,
   name: `Fantastic Beasts`,
   posterSmall: `/img/fantastic-beasts-the-crimes-of-grindelwald.jpg`,
+  trailer: `https://download.blender.org/durian/trailer/sintel_trailer-480p.mp4`
 };
 
 it(`Render SmallMovieCard`, () => {
   const tree = renderer
-    .create(<SmallMovieCard movie={movie} onCardClick={() => {}}/>)
+    .create(<SmallMovieCard movie={movie} onCardClick={() => {}}/>, {
+      createNodeMock: () => ({})
+    })
     .toJSON();
 
   expect(tree).toMatchSnapshot();
