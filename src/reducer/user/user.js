2d5d689a77ab9e7ab79bff4f58c1c5b7cdcfd314
pavelkarev <pavel.karev@pegast.ru>
2020-03-27 21:31:15 +0300

    Add User reducer

---
+++ src/reducer/user/user.js
@@ -0,0 +1,41 @@
+const AuthStatus = {
+  AUTH: true,
+  NO_AUTH: false,
+};
+
+const initialState = {
+  isAuthorized: AuthStatus.NO_AUTH,
+};
+
+const ActionType = {
+  SET_AUTH_STATUS: `SET_AUTH_STATUS`,
+};
+
+const ActionCreator = {
+  setAuthStatus: (val) => ({
+    type: ActionType.SET_AUTH_STATUS,
+    payload: val,
+  }),
+};
+
+const Operation = {
+  checkAuth: () => (dispatch, getState, api) => {
+    return api.get(`/login`)
+      .then(() => {
+        dispatch(ActionCreator.setAuthStatus(AuthStatus.AUTH));
+      });
+  },
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.SET_AUTH_STATUS:
+      return (Object.assign({}, state, {
+        isAuthorized: action.payload,
+      }));
+  }
+
+  return state;
+};
+
+export {reducer, ActionCreator, ActionType, Operation, AuthStatus};
---
+++ src/reducer/user/user.test.js
@@ -0,0 +1,40 @@
+import MockAdapter from "axios-mock-adapter";
+import {reducer, ActionType, ActionCreator, AuthStatus, Operation} from './user.js';
+import {createAPI} from '../../api.js';
+
+const api = createAPI(() => {});
+
+const initialState = {
+  isAuthorized: false,
+};
+
+it(`Reducer without params should return initial state`, () => {
+  expect(reducer(undefined, {})).toEqual(initialState);
+});
+
+it(`Reducer should set movies`, () => {
+  expect(reducer(initialState, ActionCreator.setAuthStatus(AuthStatus.AUTH))).toMatchObject({
+    isAuthorized: true,
+  });
+});
+
+describe(`Operation work correctly`, () => {
+  it(`Should make a correct API call to /login`, function () {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const authCkecker = Operation.checkAuth();
+
+    apiMock
+    .onGet(`/login`)
+    .reply(200, {});
+
+    return authCkecker(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.SET_AUTH_STATUS,
+          payload: true,
+        });
+      });
+  });
+});
