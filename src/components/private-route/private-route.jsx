5d2a0b530a96fd994eaa8df9b4fc35e8f9089f24
pavelkarev <pavel.karev@pegast.ru>
2020-03-30 16:21:19 +0300

    Add PrivateRoute component

---
+++ src/components/private-route/private-route.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Route, Redirect} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {AppRoute} from '../../routes';
+
+const PrivateRoute = ({render, path, exact = false, isAuthorized}) => (
+  <Route
+    path={path}
+    exact={exact}
+    render={(props) => {
+      return (
+        (isAuthorized) ? (
+          render(props)
+        ) : (
+          <Redirect to={AppRoute.SIGN_IN}/>
+        )
+      );
+    }}
+  />
+);
+
+PrivateRoute.propTypes = {
+  isAuthorized: PropTypes.bool.isRequired,
+  render: PropTypes.func.isRequired,
+  path: PropTypes.string.isRequired,
+  exact: PropTypes.bool.isRequired,
+};
+
+export default PrivateRoute;
